0f544f983ccae28e6ebd5a8b1b3ce81f42d187f2
Gargi00 <sharmagargi07062005@gmail.com>
2024-08-16 11:50:30 +0530

    javascript

--- 1.js
+++ 1.js
@@ -5,12 +5,12 @@ console.log("hello techno"); //to print the value
 // alert("hello gs")  //terminal pr error node ki fun nhi h html mein hoga
 
 //var let const (diff is imp) 3keywords to define variables(imp)
-//variable declared by var can be reassigned or redeclared
+// (1)variable declared by var can be reassigned or redeclared
 // var num1=20;
 // var num1=30; in this it will give no error and accepts the same name of var reassign
-// let num2=30;
+// (1)let num2=30;
 // num2=40; //variable declard with let can be reassigned but cant be redeclared
-// const num1=10;
+// (1)const num1=10;
 // const=50 //variable declared with const cannot be reassigned or redeclared
 
 var firstnum = 20; //variable
@@ -61,19 +61,19 @@ let sub = a - b;
 console.log(add)
 console.log(sub);
 
-let firstname="gan";
-let lastnme="shar";
-console.log(firstname+ '' +lastnme);
+let firstname = "gan";
+let lastnme = "shar";
+console.log(firstname + '' + lastnme);
 
-console.log(a**b)
+console.log(a ** b)
 
 // console.log(5+'5') sirf is case mein 5 bhi string bn jaeega except this in -,/,* mathematical operation hoga
 // console.log('gs'+10) will give gs10
 // console.log('gs'-10)will give  num error nan
 
 //additional assignment operators
-let x=5;
-x=x+10;
+let x = 5;
+x = x + 10;
 // x +=10;
 
 // let str="shubham";
@@ -81,29 +81,29 @@ x=x+10;
 // console.log(constr)
 // console.log(typeof constr)//type poison isme + will try to convert into number but in shubham it will not so will giv nan but if"10" than this string can be con by +
 
-let str1="5";
-let str2=+str1; //isme + string 5 ko convert krdega num
+let str1 = "5";
+let str2 = +str1; //isme + string 5 ko convert krdega num
 // console.log(5+'5'); will give 55
 
 //sub assign operator
-let y=10;
+let y = 10;
 // y-=10;
 // y*=10;
 // y/=10;
 
 //comparision operators return boolean
-let  num1=10
-let num2=10
+let num1 = 10
+let num2 = 10
 console.log(num1 == num2) //only values compare
-console.log(num1===num2) //will compare datatype and values
+console.log(num1 === num2) //will compare datatype and values
 
 //logical operators
 //let isloggedin=true
 //let isaddin=true
-let isloggedin=10; //any num greater than 0 it will return return the num written at addin as it is last
-let isaddin=20;//wherea 0 and num is there than 0 will give false
-console.log("is the user logged in signed in",isloggedin && isaddin)
-console.log(isaddin||isloggedin) //0||0 is 0,0||10 is 10
+let isloggedin = 10; //any num greater than 0 it will return return the num written at addin as it is last
+let isaddin = 20;//wherea 0 and num is there than 0 will give false
+console.log("is the user logged in signed in", isloggedin && isaddin)
+console.log(isaddin || isloggedin) //0||0 is 0,0||10 is 10
 
 //conditional statements
 //if else,switch,ternary operator
@@ -130,19 +130,142 @@ console.log(isaddin||isloggedin) //0||0 is 0,0||10 is 10
 //     console.log("coun is less than 20");
 //  }
 
-let firstnum1=prompt("enter the first num"); //input from user but prompt does not work in nodejs
-console.log(firstnum1);
+// let firstnum1 = prompt("enter the first num"); //input from user but prompt does not work in nodejs
+// console.log(firstnum1);
 //prompt the user to enter their username and password
 //if username is admin and password is 123 in numbers
 //if succcess than author successful else not
 
-let user=prompt("enter your username")
-let pass=prompt("enter your password")
-if(user==="admin" && pass===123){
-    console.log("author successful");
+// let user = prompt("enter your username")
+// let pass = prompt("enter your password")
+// if (user === "admin" && pass === 123) {
+//     console.log("author successful");
+// }
+// else {
+//     console.log("author not successful");
+// }
+//or
+// if(username==="admin"){
+//     if(password===123){
+//         console.log()
+//     }
+// }
+// elseconsole.log()
+
+//ternary operators
+// let count =20;
+// count===20? console.log("count is 20"):console.log("not 20")
+// count===20? console.log("count is 20"):count>20?console.log("the count is greater then 20"):console.log("less than 20");
+
+// let username=prompt("enter username");
+// let password=prompt("enter password");
+// (username==="admin" && password===123)?console.log("auth successful"):console.log("auth failed")
+
+//switch cases
+let daynumber = 3;
+let dayname;
+switch (daynumber) {
+    case 1:
+        dayname = "monday"
+        break; //to break loop if con is true
+    case 2:
+        dayname = "tuesday"
+        break;
+    case 3:
+        dayname = "wednesday"
+        break;
+    case 4:
+        dayname = "thursday"
+        break;
+    case 5:
+        dayname = "friday"
+        break;
+    case 6:
+        dayname = "sat"
+        break;
+    case 7:
+        dayname = "sun"
+        break;
+    default:
+        daynumber = "invaild day number"
+
 }
-else{
-    console.log("author not successful");
+console.log(dayname);
+
+//3 functions
+//func declaration
+//func expression
+// arrow function
+
+// 1)func declaration
+// function sum(){
+//     console.log("this is sum function");
+// }
+
+// 2)func exp
+// let sum =function(){
+//     console.log("this is funvc exp");
+// }
+
+//3)arrow exp
+// let sum=()=>{
+//     console.log("this is arrow function")
+// }
+
+let ad = (a, b) => {  //let add=(a,b)=>a+b use if return is single lined
+    return a + b;
 }
+let result = ad(2, 5)
+console.log(result)
+
+// 2 diff bet var,let const
+//global,local,block({}everything is block scope except in func)
+// (2)var is global scope variable but in function it becomes local scope var
+// (2)let and const are block scope variable
+// {
+//     let a=20; //it is block scope var so bahar se acces nhi hoga
+// }
+// console.log(a)
+// {
+//     var b=20; //ye global var h ye accessible rhega except in func
+// }
+// console.log(b)
+
+// function sum2(){
+//     var a=20; //it is func so cant be accessed globally accessed locally
+//     return a;
+// }
+// console.log(a) 
+
+//if sum2(){ //it can be accessed globally
+// var a=30; 
+// return a;
+// }
+//console.log(a)
+
+//array and objects
+// let arr = [10, 20, 30, 40, 50];
+// console.log(arr[0]);
+// console.log(arr[arr.length - 1]); //to access last element
+
+// nested arrays
+let nestedarray=[10,20,30,[1,2,3,4],[20,,30,40,["aaa","aab",[true,false]]]];
+let newarr=nestedarray[nestedarray.length-1.] //it will access whole last element
+let newarr1=newarr[newarr.length-1] //it will access aaa
+let newarr2=newarr1[newarr1.length-1] //it will access true
+console.log(newarr2)
+
+//console.log(arr.length)
+// arr[arr.length]="shubh";
+// console.log(arr);
+
+//objects
+// let obj={
+//     "firstname":"g",
+//     "lastname":"s"
+// }
+// console.log(obj.lastname);
+// obj.section="fsd";
+
 
 
---
+++ 2.js
@@ -0,0 +1,150 @@
+"use strict" //globally defined
+// without strict 
+// let a=10;
+// b=30;
+// console,log(b); 
+// function sum(a,a){  //duplicate value can be accepted 
+// console.log(sum func){
+// }
+// sum()
+
+// loops,strict mode,console type,array methods
+
+// for,while,do while
+// while
+// let count = 1;
+// while (count <= 10) {
+//     console.log(count);
+//     count++;
+// }
+
+
+// do while
+// do {             //it will run atleast one even if the condition is not satisfied
+//     console.log(count);
+//     count++
+// } while (count <= 5)
+
+//for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// for (let i = 0; i<=arr.length;i++) {
+//     console.log(arr[i]);
+// }
+
+// // for of and for in lopps
+// let items=["first","second","third",4,5,"sixth"]
+// for(let item of items){   //elements it will give
+//     console.log(item) ;    
+// }
+// for(let index in items){
+//     console.log(index)  //it will give the indexing
+//     console.log(items[index]); //to give indexing with each item
+// }
+
+// types of console used in html 
+// console.log("log statement")
+// console.error("to show error") //it willl show the error in red
+// console.warn("warning") //it will show warning
+// console.assert(5>10, "5 not greater than 10") //in assert if the condition false than the console will print
+// console.assert(10>5,"10 is greater than 5")   //the condition is true than console will not run
+
+let person = {
+    name: "gargi",
+    age: "20",
+    address: {
+        city: "udaipur",
+        zipcode: "313001"
+    },
+    hobbies: ("singing", "travelling")
+}
+console.dir(person)
+
+let data = [
+    {
+    name: "gargi", address: "jaipur", dept: "f50"
+},
+{
+    name:"yahoo",address:"udaipur",dept:"cse"
+},
+{
+    name:"cutie",address:"wifii",dept:"eco"
+}
+]
+console.table(data);
+
+// console.group("Group");
+// console.log("message 1");
+// console.log("m2");
+// console.log("m3");
+// console.groupEnd();
+
+// console.count("counter");
+// console.count("counter");
+// console.count("counter");
+
+// let arr =[10,20,30,40];
+// let result=arr.push(1000); //to push the element
+// console.log(result); //indexing btaega
+
+// console.log(arr);
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// let arr=[10,20,30];
+// console.log(arr.unshift(1000))
+// console.log(arr); //unshift is used to add the element at 0 th index(returns array lenth)
+
+// console.log(arr.shift()) //to remove the element from 0 the index
+// console.log(arr);
+
+// let arr1=[20,30];
+// let arr2=[10,40];
+// let result=arr2.concat(arr2);
+
+// console.log(arr1);
+// console.log(arr2);
+// console.log(concat); ///to concat both arrays but it doesnt change the original arrays it form new array
+
+// slice(it doesnt change the original array)
+// let arr=[10,20,30,40];
+//  let slicearr=arr.slice(2,5) //here ye 2 indexing se start hogi slicing whereas to end 5 likhne pr wo 4 tk hi likh kr dega
+//  console.log(arr);
+// console.log(slicearr);
+
+// splice(change the array)
+// let arr3=[10,20,30,40];
+// console.log(arr3.splice(2)); //to remove the element from back after 2 index
+// arr3.splice(2,2,"gargi") //to repace the 2 element to gargi and will remove the second element
+// // arr.splice(2,0,"gargi") to add the string but not to remove the 2 index
+// console.log(arr3);
+
+// includes
+// let arr4=[20,30,40,50]
+//     let result=arr4.includes(40); //if 40 is in the array it will return true
+//     console.log(result1);
+
+// // reverse
+// arr3.reverse();
+// console.log(arr);
+
+// join
+// let arr=[10,20,30,40,50,60];
+// let result1=arr.join("+"); //to add + between each element predsent in arrays(mostly used for the format change of date)
+// console.log(result1);
+
+// flat
+// let arr5=[10,20,[30,[40]]]
+// let result2=arr5.flat(1) //it is used to remove the nested loop if 1 than it will remove one nested array from back
+// console.log(result2)
+
+//map,filter,reduce
+let arr=[10,20,30,50,40];
+ arr.map((item)=>{      //map is func which accepts another func in its parameter so it is called  and the function present inside is callback func or anonymous function
+   return item*10;
+ })   
+ console.log(arr);
